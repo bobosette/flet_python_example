ccd85806de601c6cd12579b02c39f83bbe3eba06
Bobo <roberto.prandin@newronika.com>
2023-10-27 16:56:46 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flet/flet.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  await setupDesktop();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flet Flutter Demo',
+      home: FletApp(pageUrl: "https://deskflet.robertoprandin.repl.co/", assetsDir: '',),
+    );
+  }
+}
